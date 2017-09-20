71f5b4e1b95c6eeb1afe85fd50ce0d9c65f824b7
KEVIN DELASSUS - STUDENT <G00270791@gmit.ie>
2017-09-20 10:40:37 +0100

    Problem 2 done.

---
+++ TimeAndDate.go
@@ -0,0 +1,16 @@
+// Author: Kevin Delassus
+//Date: 20/09/17
+
+//Adapted from https://tour.golang.org/welcome/4
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("The time is", time.Now())
+}
