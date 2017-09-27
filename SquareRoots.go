dbf8cce570a245ab06e1117eb0f14ff3ee4bbbf8
KEVIN DELASSUS - STUDENT <G00270791@gmit.ie>
2017-09-27 10:48:00 +0100

    Foundations of Program

---
+++ SquareRoots.go
@@ -0,0 +1,31 @@
+//Coder: Kevin Delassus
+//Date: 27-09-2017
+//FizzBuzz - Question o
+
+package main
+
+import (
+	"fmt"
+)
+
+func getSquareRoot(x := float64, u := float64){
+	z_next := z - (((z*z) - x) / (2 * z))
+
+	return y
+}
+
+func main() {
+
+	y := 20
+	var x float64
+
+
+	for x = 1.0, x != getSQuareRoot(x,y),y = getSquareRoot(x,y){
+
+		
+	}
+
+	fmt.Println("")
+
+
+}
