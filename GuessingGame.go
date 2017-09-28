d4ebb1f77713cec194ee3c04c94b38543468db95
Kevin Delassus <g00270791@gmit.ie>
2017-09-28 18:14:09 +0100

    Program Working more comments needed

--- GuessingGame.go
+++ GuessingGame.go
@@ -6,83 +6,89 @@
 package main
 
 import (
-	//"bufio"
 	"fmt"
-	//"os"
 	"math/rand"
 )
 
-var entry [10]int
+//Array to store guesses
+var entry [50]int
 
 func main() {
 
-	var i int
+	var guess int
 	var ranNum int
 	count := 0
 	check := true
 
-	
-	
+	//Generating a random number between 1 & 100
 	ranNum = rand.Intn(100)
 
-	fmt.Print(ranNum)
-   
 	fmt.Println("Please Enter a Single Number between 1 & 100: ")
-	fmt.Println("---------------------")
+	fmt.Println("---------------------------------")
 
-	for i != ranNum{
+	for guess != ranNum{
 
 		fmt.Print("-> ")
-		fmt.Scan(&i)
+		//Taking user input
+		fmt.Scan(&guess)
 		
-		
-		if i > ranNum{
+		//Checking if guess is greater then the random number
+		//If it is then message is returned saying you are too high
+		if guess > ranNum{
 
-			check = CheckArray(i,count)
+			check = CheckArray(count,guess)
 
 			if check == true{
 				fmt.Println("You are to High!")
 				
 				//fmt.Println(entry[count])
 				count++
-			}
+			}else{fmt.Println("You entered that number already")}
 		}
-		if i < ranNum{
+		//Checking if guess is less then the random number
+		//If it is then message is returned saying you are too low
+		if guess < ranNum{
+			
+			//Checking if the number has been entered before
+			 check = CheckArray(count,guess)
 
-			 check = CheckArray(i,count)
-
+			 //If Number hasnt been entered before then message is printed and count is incremented
 			if check == true{
 				fmt.Println("You are to Low!")
 				
 				//fmt.Println(entry[count])
 				count++
-			}
-			
+			}else{fmt.Println("You entered that number already")}
 		}
-		if i == ranNum{
+		//if guess equaled to the random number then the program breaks out of the for loop
+		if guess == ranNum{
 			break
 		}
 	}
+	//Message displaying that you have won the game
 	fmt.Println("Correct You Win")
+	//Calling function to check how many times user has guessed
 	CountTimes(count)
-
-	
 }
-func CheckArray(i int, guess int){
+//Function to check the array
+func CheckArray(count int, guess int) bool{
 
-	entry[i] = guess
+	entry[count] = guess
 
 	for x := 0; x < count; x++{
-		if entry[i] == guess{
+		if entry[x] == guess{
 			return false
-		}else {return true}
+		}
 	}
+	return true
 }
+//Function to check how many times user guessed
 func CountTimes(c int){
 	if c == 0{
 		fmt.Println("Woow you guessed correct on the first go!! WELL DONE!!")
 	}else{
+		c++
 		fmt.Println("You Guessed ", c," times!!")
 	}
-	
+	fmt.Println("---------------------------------")
 }
