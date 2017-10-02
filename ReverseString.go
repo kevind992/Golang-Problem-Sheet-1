2edf7f2baeeb0fbb49286469b3591e0f1396e17a
Kevin Delassus <g00270791@gmit.ie>
2017-10-02 13:52:34 +0100

    Program working comments needed

---
+++ ReverseString.go
@@ -0,0 +1,29 @@
+//Coder: Kevin Delassus
+//Date: 02/10/17	
+//Source: https://stackoverflow.com/questions/1752414/how-to-reverse-a-string-in-go
+
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+
+	//String which needs to be reversed
+	string := "Kevin"
+
+	//Calling Funstion and Displaying Answer
+	fmt.Println(Reverse(string))
+
+}
+
+//Function which reverces the string
+func Reverse(s string) string {
+
+    runes := []rune(s)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
+}
