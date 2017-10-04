2e69b5f4e7f1461b07658101802b6b63c89661ca
Kevin Delassus <g00270791@gmit.ie>
2017-10-04 20:10:31 +0100

    Program Working Comments needed

--- FactorialDigitSum.go
+++ FactorialDigitSum.go
@@ -1,47 +1,62 @@
 //Question 4
-//Coder: Kevin Delassus
-//Date: 26/09/17
-//Discription: Write a function that calculates the sum of the digits of the factorial of a number.
-// n! means n x (n − 1) ... x 3 x 2 x 1. 
-//For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800, 
-//and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27. 
+//Author: Kevin Delassus
+//Write a function that calculates the sum of the digits of the factorial of a number.
+//n! means n x (n − 1) ... x 3 x 2 x 1. For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
+//and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 //Then find the sum of the digits in the number 100!.
+//Adapted From: https://github.com/chirag04/golang-project_euler/blob/master/p020.go
 
 
 package main
 
 import (
 	"fmt"
-	"math/big"
+	//"os"
 )
+ 
 
-func main() {
+func main(){
 
+	//Setting variables
+	var answer int
+
+	//Change this value to get a different answer
+	check := 100
 	
-	fmt.Println(factorial(10)) //100 can be changed to any number
-	sum := big.NewInt(0)
-	n := new(big.Int) //to store the factorial
-	fact := (factorial(100))
+	//Running calFactorial funtion
+    answer = calFactorial(check)
 
-	//func (z *Int) DivMod(x,y,m*Int)(*Int, *Int)
-	//sets z to quotient x div y and m to modulus x mod y and returns
-	//the pair (z,m) where y is not 0.
+	//Printing out answer
+	fmt.Print("The Factorial sum of ",check ," is: ", answer)
+}
+
+func calFactorial(num int) int {
 	
-	for fact.BitLen() > 0 {
-		_, n := fact.DivMod(fact, new(big.Int).SetUint64(uint64(10)), n)
-		sum = sum.Add(sum, n)
+	//Setting variables
+    sum := 0;
+    
+    nums := [200] int{};
+    nums[0] = 1;
+	
+	//double for loop calculating the factorial sum and putting the individual digits into a slice
+    for i := 2; i <= num; i++ {
+        
+    	for x := 0; x < len(nums); x++ {
+            
+            nums[x] *= i;
+            //
+    		if x > 0 && nums[x - 1] > 9 {
+    			nums[x] += int(nums[x - 1] / 10);
+    			nums[x - 1] %= 10;
+    		}
+    	}
 	}
-	//_, acts like a place holder. BitLen breaks the result into individual digits
-	fmt.Println(sum)
+	
+	//for adding all the numbers together to get final answer
+    for i := 0; i < len(nums); i++ {
+    	sum += nums[i];
+	}
+	//Returning the answer
+    return sum;
 }
-func factorial(n int64) *big.Int {
-	if n < 0 {
-		return big.NewInt(1)
-	}
-	if n == 0 {
-		return big.NewInt(1)
-	}
-	bigN := big.NewInt(n)
-	return bigN.Mul(bigN, factorial(n-1))
 
-}
